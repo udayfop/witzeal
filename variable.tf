59031ed7dfb186b49d6d9708c8a8237d2ef16c2c
UDAY PRATAP SINGH <31985102+udayfop@users.noreply.github.com>
2023-04-11 00:41:09 +0530

    Update variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+variable "key_name" {
+  description = "keys to connect to ec2 instance"
+  default     =  "udaytest"
+}
